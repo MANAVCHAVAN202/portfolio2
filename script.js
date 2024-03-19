f101556997ea667bfa57d62bc80c45611ddfec75
MANAVCHAVAN202 <163837174+MANAVCHAVAN202@users.noreply.github.com>
2024-03-19 09:28:16 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function navigateTo(page) {
+  //   fetch(page)
+  //     .then((response) => response.text())
+  //     .then((html) => {
+  //       document.getElementById("content").innerHTML = html;
+  //     })
+  //     .catch((error) => console.error("Error loading the page:", error));
+  console.log(page);
+  location.href = page;
+}
+
+// // Initial load of home page
+// navigateTo("home.html");
